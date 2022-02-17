8684d558ba26ed074ff5b2c9a743b11c4d3078a5
gregoconst <gregorio.constante12@hotmail.com>
2022-02-17 07:32:38 -0300

    console logs

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -87,7 +87,7 @@ router.get("/videogames", async (req, res) => {
   }
 });
 
-router.get("/videogames/:id", async (req, res, next) => {
+router.get("/videogames/:id", async (req, res) => {
   const { id } = req.params;
 
   try {
